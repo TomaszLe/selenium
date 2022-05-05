9dc5a9e3ceaf134003a4523007d56467bb3bffa8
Tomasz Letkiewicz <tk.letkiewicz@gmail.com>
2022-05-05 16:18:36 +0200

    firefox driver

--- src/test/java/pl/testeroprogramowania/utils/DriverFactory.java
+++ src/test/java/pl/testeroprogramowania/utils/DriverFactory.java
@@ -3,10 +3,14 @@ package pl.testeroprogramowania.utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
     public static WebDriver getDriver() {
-        WebDriverManager.chromedriver().setup();
-        return new ChromeDriver();
+        //WebDriverManager.chromedriver().setup();
+        //return new ChromeDriver();
+        WebDriverManager.firefoxdriver().setup();
+        return new FirefoxDriver();
+
     }
 }
